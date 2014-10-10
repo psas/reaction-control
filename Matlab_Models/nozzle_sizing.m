b336e028ae415b0b2c8b1f74cafd46b88e0b0b43
unknown <jlouke@MCAE25.ds.cecs.pdx.edu>
2014-10-10 12:33:56 -0700

    more stuff and things

---
+++ Matlab_Models/nozzle_sizing.m
@@ -0,0 +1,9 @@
+y=1.4;%ratio of specific heats
+for pcpsi=50%[40:.1:100] ;%Chamber Pressure [Psi]
+pc=pcpsi/1.450377E-4%chamber pressure [Pa]
+pe=14.7/1.450377E-4;%atmosphereic pressure at sea level [Pa]
+Me=sqrt(2/(y-1)*((pc/pe)^((y-1)/y)-1))%Mach number at nozzle exit
+e=1/Me*sqrt((2/(y+1)*(1+(y-1)/2*Me^2))^((y+1)/(y-1)))%Area ratio Ae/At
+hold all
+plot(pc,e);
+end
