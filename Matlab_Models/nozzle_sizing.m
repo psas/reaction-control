7dc66ba7faa90a16228cc1e56ff248ea36149cf0
7deeptide <7deeptide@gmail.com>
2014-10-10 12:36:07 -0700

    Update nozzle_sizing.m

--- Matlab_Models/nozzle_sizing.m
+++ Matlab_Models/nozzle_sizing.m
@@ -1,5 +1,5 @@
 y=1.4;%ratio of specific heats
-for pcpsi=50%[40:.1:100] ;%Chamber Pressure [Psi]
+for pcpsi=[40:.1:100] ;%Chamber Pressure [Psi]
 pc=pcpsi/1.450377E-4%chamber pressure [Pa]
 pe=14.7/1.450377E-4;%atmospheric pressure at sea level [Pa]
 Me=sqrt(2/(y-1)*((pc/pe)^((y-1)/y)-1))%Mach number at nozzle exit
