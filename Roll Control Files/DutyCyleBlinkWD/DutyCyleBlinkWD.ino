0c5b7c9bc2ebead7a04e7a42df620d382a716708
brntly <brentleywiles@brentleys-MacBook-Air.local>
2015-04-03 15:08:47 -0700

    Duty cycle without Delay

--- Roll Control Files/DutyCyleBlinkWD/DutyCyleBlinkWD.ino
+++ Roll Control Files/DutyCyleBlinkWD/DutyCyleBlinkWD.ino
@@ -2,32 +2,41 @@
 /*
 This Code allows for variation in duty cycle for BlinkWithoutDelay
 */
-const unsigned int onTime = 500;
-unsigned long previousMillis=0;
+
+const unsigned int onTime = 1000;  // change overall PWM cycle time here!!!!
+unsigned long previousMillis=0;  
 int interval = onTime;
 boolean LED13state = true;
- 
 
 void setup() {
   pinMode(13, OUTPUT);
   Serial.begin(9600);
 }
  
-void loop() {
-  
-  float val = analogRead(0);
-  val = map(val, 0, 1023, 0, 100);
-  float sal = val/100;
+
+
+void blinkwd(){ //controls duty cycle only 
+  double val = map(analogRead(0), 0, 1023, 0, 100); 
+  val = val/100;
   digitalWrite(13, LED13state);
   unsigned long currentMillis = millis(); 
   if ((unsigned long)(currentMillis - previousMillis) >= interval) {
     if (LED13state) {
-      interval = sal*onTime;
+      interval = onTime-val*onTime;
     } else {
-      interval = onTime-sal*onTime;
+      interval = val*onTime;
     }
     LED13state = !(LED13state);
     previousMillis = currentMillis;
   }
-  Serial.println(sal);
+  Serial.println(val);
+  
+} 
+ 
+ 
+ 
+void loop() {
+  
+  blinkwd();
+  
 }
